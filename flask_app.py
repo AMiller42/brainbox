5fdf7b870eeb36c02a98147abaea8afc00eede54
Aaron Miller <aarmil04@gmail.com>
2021-07-08 15:22:47 -0400

    pls fix?

--- flask_app.py
+++ flask_app.py
@@ -58,8 +58,7 @@ def execute():
 
             y.write(ret[1])
     with open(f"sessions/{session}/.stdout", "r", encoding="utf-8") as x:
-        with open(f"sessions/{session}/.stderr", "r", encoding="utf-8") as y:
-            val = {"stdout": x.read(), "stderr": y.read()}
+            val = "stdout": x.read()
     shutil.rmtree(f"sessions/{session}", ignore_errors=True)
     return val
 
